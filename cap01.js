6ddc9bd988d8c7ae239389f22a07098ab338b2df
Samuel Lima de Farias <samuel.lima@ebserh.gov.br>
2022-09-01 13:57:04 -0300

    quase finalizando objetos

--- cap01.js
+++ cap01.js
@@ -80,6 +80,146 @@ console.log("subString(2, 10): " + nome.substring(2, 10))
 //TIPOS DE REFERENCIA
 //CRIANDO OBJETOS
 
+var objeto_01 = new Object();
+var objeto_02 = objeto_01;
+
+console.log(objeto_01 == objeto_02); //true
+console.log(objeto_01 === objeto_02); //true
+
+
+//REMOVENDO A REFERNEICA A OBJETOS
+objeto_02 = null;
+console.log(objeto_01 == objeto_02); //false
+console.log(objeto_01 === objeto_02); //false
+
+
+//ADICIONADO OU REMOVENDO PROPRIEDADES
+
+var objeto_03 = new Object();
+var objeto_04 = objeto_03;
+
+objeto_03.myCustomProperty = "Nova propiedade";
+console.log(objeto_04.myCustomProperty); // Nova propriedade
+
+
+//INSTANCIANDO TIPOS PRÓPRIOS
+    //tipos próprios
+    /**
+     * Array, Date, Error, Function, Object, RegExp
+     */
+    var itens = new Array();
+    var dataAgora = new Date();
+
+    console.log(itens); //[]
+    console.log(dataAgora); //2022-09-01T14:04:15.292Z
+
+    var nomesAdicionar = new Array("Samuel", "Lima", "De", "Farias");
+    console.log(nomesAdicionar); //[ 'Samuel', 'Lima', 'De', 'Farias' ]
+
+
+    itens.push("SAMUEL");
+    itens.push("RUANNA");
+    console.log(itens); //[ 'SAMUEL', 'RUANNA' ]
+
+
+//FORMAS LITERAIS
+    //LITERAIS DE OBJETOS E ARRAYS
+    console.log("----------------------------------------------");
+    var livro = {
+        nome : "Matematica",
+        ano : 2022
+    };
+
+    console.log("Objeto Completo: " + livro); //[object Object]
+    console.log("Nome do livro: " + livro.nome); // Matematica
+    console.log("Ano do livro: " + livro.ano); // 2022
+
+    console.log("----------------------------------------------");
+    var livro_02 = {
+        "nome" : "Fisica",
+        "ano" : 2021
+    };
+
+    console.log("Objeto Completo: " + livro_02); //[object Object]
+    console.log("Nome do livro_02: " + livro_02.nome); // Fisica
+    console.log("Ano do livro_02: " + livro_02.ano); // 2021
+
+    console.log("----------------------------------------------");
+    var livro_03 = new Object();
+    livro_03.nome = "Geografia";
+    livro_03.ano = 2019;
+
+    console.log("Objeto Completo: " + livro_03); //[object Object]
+    console.log("Nome do livro_03: " + livro_03.nome); // Fisica
+    console.log("Ano do livro_03: " + livro_03.ano); // 2021
+
+    console.log("----------------------------------------------");
+
+    //ARRAYS
+    var cores = ['Azul', "Branco", "verde"];
+    var cores_01 = new Array("Azul", 'Branco', "verde");
+
+    console.log(cores[0]); //Azul
+    console.log(cores_01[2]); //verde
+
+    console.log("----------------------------------------------");
+
+    //LITERAIS FUNCTIONS
+    function soma(a, b){
+        return a+b;
+    }
+    console.log("Soma 01: " + soma(2,2));
+
+    console.log("----------------------------------------------");
+
+
+    //LITERAIS FUNCOES REGULARES
+
+    var nnumbers = /\d+/g;
+    var nnumbers = new RegExp("\\d+", "g");
+
+    //ACESSO A PROPRIEDADES
+
+    var numerosArrays = [];
+    numerosArrays.push(1234); 
+    console.log(numerosArrays); // 1234
+
+    var numerosArrays_02 = [];
+    numerosArrays_02["push"](4567);
+    console.log(numerosArrays_02); // 4567
+
+    console.log("----------------------------------------------");
+
+    console.log("----------------------------------------------");
+
+    console.log("----------------------------------------------");
+
+    console.log("----------------------------------------------");
+
+    console.log("----------------------------------------------");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
